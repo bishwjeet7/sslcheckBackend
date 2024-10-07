0ee11b273942c542efb50d9692a66bd65424d346
Bishwjeet kumar <66054602+bishwjeet7@users.noreply.github.com>
2024-10-07 22:26:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 app.use(cors({
-  origin: FRONTEND_URL,
+  origin: '*',
   methods: ['POST'],
   allowedHeaders: ['Content-Type'],
 }));
